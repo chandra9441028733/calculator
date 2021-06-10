e725e5b7f2925197ed4adda97066b65ea7fc628d
chandra9441028733 <chandra9441028733@gmail.com>
2021-06-10 06:05:56 +0000

    update

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Calculator extends HttpServlet {
+pblic class Calculator extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
